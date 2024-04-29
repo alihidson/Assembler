d5fce8bcaac01b78cd24bdc841b5ab0a7b031585
alihidson <amakhavan81@gmail.com>
2024-04-29 18:08:18 +0330

    add sll, xor, or, and instruction

--- sll, xor, or, and/cmake-build-debug/cmake_install.cmake
+++ sll, xor, or, and/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/ali/Main/Documents/Source/Assembler/addi
+# Install script for directory: /Users/ali/Main/Documents/Source/Assembler/sll, xor, or, and
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/ali/Main/Documents/Source/Assembler/addi/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/ali/Main/Documents/Source/Assembler/sll, xor, or, and/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
